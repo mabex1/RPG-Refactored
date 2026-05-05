f5ac5ceceedc845b25de637d63b38f9077cf79e1
mabex1 <iskanderiskander49@gmail.com>
2026-05-05 21:16:25 +0500

    Update comment in program.cs

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -7,8 +7,8 @@ namespace RPG;
 //I am learning c#, so code is not really well, but better than the old one
 //You can check the old one in my github page. 
 //https://github.com/mabex1
-// Old version:
-// New version:
+// Old version: https://github.com/mabex1/RPG-Monolith
+// New version: https://github.com/mabex1/RPG-Refactored
 // Also, game fully in russian. English version is not planned. It's just a stupid console rpg. Keep that in mind.
 class Program
 {
